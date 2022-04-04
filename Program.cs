c43864e09c7ed9bbeacab145f830e4fa18ef3721
Brian Marth <marthbrian@gmail.com>
2022-04-04 09:26:41 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello .NET core C# 2022 BMarth");
+        }
+    }
+}
